918683b8cd2902228e7ccc9ad28fcfc420ba93ac
T-Mac <zhuxi@1haitao.com>
2017-03-30 16:39:27 +0800

    el to org

--- init.el
+++ init.el
@@ -13,35 +13,11 @@
                   ; when Smex is auto-initialized on its first run.
 ;(global-set-key (kbd "M-x") 'smex)
 
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
-(add-to-list 'load-path "~/.emacs.d/lisp/")
+(require 'org-install)
+(require 'ob-tangle)
+(org-babel-load-file (expand-file-name "zhuxi.org" user-emacs-directory))
 
-;;open init file
-(defun open-my-init-file()
-  (interactive)
-  (find-file "~/.emacs.d/init.el"))
-
-(require 'init-packages)
-(require 'init-ui)
-(require 'init-better-defaults)
-(require 'init-keybindings)
-(require 'org)
-
-
-(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
-
-(load-file custom-file)
-
-;-turn off electric-indent-mode
-;(electric-indect-mode 1)
-
-
-(setq mac-option-modifier 'meta)
 
 
---
+++ zhuxi.el
@@ -0,0 +1,25 @@
+
+(package-initialize)
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+;;open init file
+(defun open-my-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(require 'init-packages)
+(require 'init-ui)
+(require 'init-better-defaults)
+(require 'init-keybindings)
+(require 'init-org)
+
+
+(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
+
+(load-file custom-file)
+
+;-turn off electric-indent-mode
+;(electric-indect-mode 1)
+
+
+(setq mac-option-modifier 'meta)
