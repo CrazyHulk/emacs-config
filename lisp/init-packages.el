929561b402d6b7c4e490850e457bad64efc9d641
朱熙 <zhuxi@bilibili.com>
2019-11-18 10:58:57 +0800

    修改 which-key 显示时间

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -109,6 +109,9 @@
 ;(require 'powerline)
 ;(powerline-default-theme)
 
+(setq which-key-show-early-on-C-h t)
+(setq which-key-idle-delay 0.01)
+(setq which-key-idle-secondary-delay 0.05)
 (which-key-mode)
 ;(setq which-key-side-window-location 'right)
 
